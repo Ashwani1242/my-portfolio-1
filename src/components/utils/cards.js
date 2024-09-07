2afbfccb5250f5ecba59b62c90068cfffd49b02e
Ashwani1242 <ashwanigupta1242@gmail.com>
2024-09-07 21:00:03 +0530

    Changes Committed on 2024-09-07, 20:59

--- src/components/utils/cards.js
+++ src/components/utils/cards.js
@@ -1,6 +1,12 @@
 
 export const cards = [
     {
+        url: "/images/projects/sonic-ring-run.png",
+        link: 'https://ashwani1242.github.io/Sonic-Ring-Run/',
+        title: "Sonic Ring Run",
+        id: 4,
+    },
+    {
         url: "/images/projects/sticky_notes.png",
         link: 'https://sticky-notes-tdbs.netlify.app',
         title: "Sticky Notes",
@@ -9,7 +15,8 @@ export const cards = [
     {
         url: "/images/projects/mine_sweeper.png",
         title: "Mine Sweeper",
-        link: 'https://play.google.com/store/apps/details?id=com.thedevbellowstairs.mine_sweeper',
+        // link: 'https://play.google.com/store/apps/details?id=com.thedevbellowstairs.mine_sweeper',
+        link: 'https://thedevbellowstairs.itch.io/mine-sweeper',
         id: 2,
     },
     {
@@ -27,13 +34,15 @@ export const cards = [
     {
         url: "/images/projects/game_of_life.png",
         title: "Conway's Game of Life",
-        link: 'https://play.google.com/store/apps/details?id=com.CarrotaniaLaunge.GameOfLife',
+        // link: 'https://play.google.com/store/apps/details?id=com.CarrotaniaLaunge.GameOfLife',
+        link: 'https://thedevbellowstairs.itch.io/game-of-life',
         id: 1,
     },
     {
         url: "/images/projects/2048.png",
         title: "2048",
-        link: 'https://play.google.com/store/apps/details?id=com.CarrotaniaLaunge.Game2048',
+        // link: 'https://play.google.com/store/apps/details?id=com.CarrotaniaLaunge.Game2048',
+        link: 'https://thedevbellowstairs.itch.io/2048',
         id: 5,
     },
     
